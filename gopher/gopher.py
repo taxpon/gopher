3985c8965c7f56daf5387ce848cfc33c7480418e
Takuro Wada <taxpon@gmail.com>
2017-02-03 09:35:55 +0900

    chore: refactor

--- gopher/gopher.py
+++ gopher/gopher.py
@@ -1,29 +1,34 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+from typing import List
 import os
 import click
 import requests
 from collections import OrderedDict
 
 
-class GopherIcon(object):
-    def __init__(self, output="gopher.png", overwrite=False):
+API_URL = "https://gopherize.me/api/"
+
+
+class Gopher(object):
+    def __init__(self, output="gopher.png", overwrite=False, simple=False):
         self.categories = OrderedDict()
         self.output = output
         self.overwrite = overwrite
-        self.get_artwork()
+        self.simple = simple
+        self.binary = None
+        self.__get_artwork()
 
-    def get_artwork(self):
-        resp = requests.get("https://gopherize.me/api/artwork")
+    def __get_artwork(self):
+        # type: () -> None
+        resp = requests.get(API_URL + "artwork")
         j = resp.json()
         for cat in j['categories']:
             self.categories[cat['id']] = cat['images']
 
-    def __get_category_items(self, cat_name):
-        pass
-
     def __build_params(self):
+        # type: () -> List[str]
         import random
         id_list = list(self.categories.keys())
         params = []
@@ -31,15 +36,20 @@ class GopherIcon(object):
             if _id in ["artwork/010-Body", "artwork/020-Eyes"]:
                 c = random.choice(self.categories[_id])
             else:
-                c = random.choice(self.categories[_id] + [None])
+                if self.simple and random.random() > 0.8:
+                    c = random.choice(self.categories[_id] + [None])
             if c:
                 params.append(c['id'])
         return params
 
     def get_image(self):
+        # type: () -> str
         params = {"images": "|".join(self.__build_params())}
         resp = requests.get("https://gopherize.me/api/render", params=params)
+        self.binary = resp.content
+        return resp.content
 
+    def save(self):
         filename = self.output
         if not self.overwrite:
             c = 1
@@ -49,14 +59,19 @@ class GopherIcon(object):
                 c += 1
 
         with open(filename, "wb") as f:
-            f.write(resp.content)
+            f.write(self.binary)
 
 
 @click.command()
 @click.option('--output', '-o', default='gopher.png', help='Output file name (png).')
 @click.option('--overwrite', '-w', is_flag=True, help='Overwrite if the same name file already exists.')
-def main(output, overwrite):
-    GopherIcon(output, overwrite).get_image()
+@click.option('--simple', '-s', is_flag=True, help='Get simple gopher.')
+def main(output, overwrite, simple):
+    # type: (str, bool, bool) -> None
+    gopher = Gopher(output, overwrite, simple)
+    gopher.get_image()
+    gopher.save()
+
 
 if __name__ == "__main__":
     main()
